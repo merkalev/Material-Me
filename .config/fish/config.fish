d8b0bc7b8b500787532fb601fa92124344c6b9f2
XxsansxV <93177301+XxsansxV@users.noreply.github.com>
2026-06-25 17:35:03 +0700

    no idea what i did to config.fish tbh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 alias dotgit='git --git-dir=$HOME/.dotfiles-backup --work-tree=$HOME'
 alias pls='sudo'
 alias bigfetch="fastfetch -c all.jsonc"
+alias tinyfetch="fastfetch -c small.jsonc"
 
 # overwrite greeting
 # potentially disabling fastfetch
@@ -10,6 +11,10 @@ alias bigfetch="fastfetch -c all.jsonc"
 # I tried delaying fish_greeting here so that it would fit, turns out it wasn't because of the window size lmao
 # function fish_greeting
 #     sleep 0.05 && fastfetch
+# end
+# #
+# function fish_greeting
+#     tinyfetch
 # end
 
 # thefuck --alias | source
