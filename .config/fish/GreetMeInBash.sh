78fe352a355845e16ab13dd36ed1169878504758
XxsansxV <93177301+XxsansxV@users.noreply.github.com>
2026-06-25 21:46:07 +0700

    the GreetMeInBash script can now send thru mako

--- .config/fish/GreetMeInBash.sh
+++ .config/fish/GreetMeInBash.sh
@@ -1,40 +1,99 @@
 #!/usr/bin/env bash
+ 
+greet_function() {
+  # Colors bro
+  RED='\e[31m'
+  GREEN='\e[32m'
+  YELLOW='\e[33m'
+  CYAN='\e[36m'
+  MAGENTA='\e[35m'
+  RESET='\e[0m'
 
-# Colors bro
-RED='\e[31m'
-GREEN='\e[32m'
-YELLOW='\e[33m'
-CYAN='\e[36m'
-MAGENTA='\e[35m'
-RESET='\e[0m'
+  # Kickass easter egg
+  # Nah, I just thought you couldn't create an empty array so i did this instead, lol.
+  greetings=("${YELLOW}Oops!${RESET} you found me! ${RED}ඞ${RESET}")
 
-# Kickass easter egg
-greetings=("${YELLOW}Oops!${RESET} you found me! ${RED}ඞ${RESET}")
+  # Add more greetings here!
+  # also, PLEASE use ${RESET} after every, every single color invocation. otherwise  you wouldn't want mako to show <span dniuqwfouq> do you?
+  greetings+=("Don't bet your life on perfection.")
+  greetings+=("You're using ${CYAN}Arch${RESET}, BTW" "${CYAN}CachyOS${RESET}, BTW")
+  greetings+=("Go get those bugs!")
+  greetings+=("You must've be tired configuring this, lol.")
+  greetings+=("Just ${YELLOW}Yank it all into registy +,${RESET} bro.")
+  greetings+=("${GREEN}'Don't be evil'${RESET} said the ${RED}villain${RESET} himself")
+  greetings+=("Yo, what's the abbr. of ${GREEN}Digital Audio Workstation Guy${RESET} again?")
+  greetings+=("WAS THAT THE ${YELLOW}BYTE OF '26???${RESET}")
+  greetings+=("Hello everybody my name is ${MAGENTA}WELCOME${RESET}")
+  greetings+=("Sudo ${CYAN}pacman${RESET}-${RED}get${RESET} install ${CYAN}arch${RESET}${RED}bian-bsd${RESET}")
+  greetings+=("Don't listen to the guy who says ${MAGENTA}'Ela desce ela sobe'${RESET}")
+  greetings+=("Whatever you do at the crossroads, ${YELLOW}keep moving forward.${RESET}")
+  greetings+=("${CYAN}Hyprlarp${RESET}")
+  greetings+=(":v")
+  greetings+=("That one guy who is ${RED}ACTUALLY${RESET} worse than the 67 kid:")
+  greetings+=("DO NOT BUY THIS KEYBO-")
+  greetings+=("[sudo] password for XxcharaxV:")
+  greetings+=("Stay ${YELLOW}DETERMINED${RESET}")
+  greetings+=("Du sollte einfach ${YELLOW}spazz machen :)${RESET}")
+  greetings+=("${MAGENTA}DU BIST GUT GENUGGGG${RESET}")
+  greetings+=("${CYAN}'This entire time... it was a symlink?'${RESET} ${RED}'Always has been.'${RESET}")
+  greetings+=("Save the ${RED}sin,${RESET} face the ${MAGENTA}E.G.O${RESET}")
 
-# Add more greetings here!
-greetings+=("Don't bet your life on perfection.")
-greetings+=("You're using ${CYAN}Arch${RESET}, BTW" "${CYAN}CachyOS${RESET}, BTW")
-greetings+=("Go get those bugs!")
-greetings+=("You must've be tired configuring this, lol.")
-greetings+=("Just ${YELLOW}Yank it all into registy +,${RESET} bro.")
-greetings+=("${GREEN}'Don't be evil'${RESET} said the ${RED}villain${RESET} himself")
-greetings+=("Yo, what's the abbr. of ${GREEN}Digital Audio Workstation guy again?${RESET}")
-greetings+=("WAS THAT THE ${YELLOW}BYTE OF '26???${RESET}")
-greetings+=("Hello everybody my name is ${MAGENTA}WELCOME${RESET}")
-greetings+=("Sudo ${CYAN}pacman${RESET}-${RED}get${RESET} install ${CYAN}arch${RED}bian-bsd${RESET}")
-greetings+=("Don't listen to the guy who says ${MAGENTA}'Ela desce ela sobe'$RESET")
-greetings+=("Whatever you do at the crossroads, ${YELLOW}keep moving forward.${RESET}")
-greetings+=("${CYAN}Hyprlarp${RESET}")
-greetings+=(":v")
-greetings+=("That one guy who is ${RED}ACTUALLY${RESET} worse than the 67 kid:")
-greetings+=("DO NOT BUY THIS KEYBO-")
-greetings+=("Sudo password for XxcharaxV:")
-greetings+=("Stay ${YELLOW}DETERMINED${RESET}")
-greetings+=("Du sollte einfach ${YELLOW}spazz machen${RESET} :)")
-greetings+=("${MAGENTA}DU BIST GUT GENUGGGG${RESET}")
+  # this just counts how many items there are in the greetings array, then picks one of the indexes.
+  howmanyGreets=${#greetings[@]}
+  whichonedidBashpickforya=$(shuf -i 0-$((howmanyGreets - 1)) -n 1)
 
-howmanyGreets=${#greetings[@]}
-whichonedidBashpickforya=$(shuf -i 0-$((howmanyGreets - 1)) -n 1)
+  # echo ""
+  echo -e "${greetings[${whichonedidBashpickforya}]}" 
+  # BashGreeted="${greetings[${whichonedidBashpickforya}]}"
+}
 
-echo ""
-echo -e "${greetings[${whichonedidBashpickforya}]}"
+case "$1" in
+  help|--help|-h)
+    echo ""
+    echo "Usage: GreetMeBash [ help | notify | greet ]"
+    echo "--------------------------------------------"
+    echo "=-=[ help ] shows this page, duh"
+    echo "=-=[ notify ] sends output thru notify-send"
+    echo "=-=[ greet ] sends the output through the terminal directly."
+    echo ""
+    ;;
+
+  notify)
+    echo "Running greet_function..."
+    BashGreeted=$(greet_function)
+
+    BashGreetedMakoGreetedU=$(echo -e "$BashGreeted" | sed \
+      -e "s/\x1b\[31m/<span foreground='red'>/g" \
+      -e "s/\x1b\[32m/<span foreground='green'>/g" \
+      -e "s/\x1b\[33m/<span foreground='yellow'>/g" \
+      -e "s/\x1b\[35m/<span foreground='magenta'>/g" \
+      -e "s/\x1b\[36m/<span foreground='cyan'>/g" \
+      -e "s/\x1b\[0m/<\/span>/g")
+    
+    echo "Trying to send to notify-send..."
+    notify-send "Hello :)" "$BashGreetedMakoGreetedU"
+    ;;
+
+  greet)
+    # echo "Running greet_function..."
+    echo ""
+    greet_function
+    echo ""
+    # echo "that SHOULD'VE worked. If not, idk man"
+    ;;
+  "")
+    echo ""
+    echo "Put params, mate. Idk use GreetMeBash -h something-something????"
+    echo ""
+    exit 1
+    ;;
+  *)
+    echo ""
+    echo "Yo dude, READ THE HELP PAGE!!! $1 is NOT a parameter. full stop."
+    echo ""
+    echo "hey, sorry about that. he's a bit angry. do GreetMeBash -h to learn the params."
+    echo ""
+    exit 1
+    ;;
+esac
+
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,7 @@ alias dotgit='git --git-dir=$HOME/.dotfiles-backup --work-tree=$HOME'
 alias pls='sudo'
 alias bigfetch="fastfetch -c all.jsonc"
 alias tinyfetch="fastfetch -c small.jsonc"
+alias GreetmeBash="bash /home/xxsansxv/.config/fish/GreetMeInBash.sh"
 
 # overwrite greeting
 # potentially disabling fastfetch
@@ -19,7 +20,7 @@ alias tinyfetch="fastfetch -c small.jsonc"
 #
 
 function fish_greeting
-    bash /home/xxsansxv/.config/fish/GreetMeInBash.sh
+    GreetmeBash
 end
 
 # thefuck --alias | source
