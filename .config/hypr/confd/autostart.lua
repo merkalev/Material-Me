0b530c822d369f6b81df06d34683b444330dde4c
XxsansxV <93177301+XxsansxV@users.noreply.github.com>
2026-06-27 14:13:44 +0700

    screw swaync the classes are too mumbo jumbo for me

--- .config/hypr/confd/autostart.lua
+++ .config/hypr/confd/autostart.lua
@@ -17,7 +17,7 @@ hl.on("hyprland.start", function ()
     hl.exec_cmd("fcitx5 -d --replace")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("systemctl --user start xdg-desktop-portal-wlr")
-    hl.exec_cmd("swaync")
+    -- hl.exec_cmd("mako")
     -- hl.exec_cmd("mako")        -- uncomment if mako isn't already a systemd user service
     -- hl.exec_cmd(programs.terminal)
     hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
